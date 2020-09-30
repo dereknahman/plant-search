51e5bfce690c469d2f152c692ef1ab1580cb7bbf
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-09-30 21:30:17 +0100

    a friend gave me a link to a magical CORS workaround and IT WORKED

--- src/api/trefle.js
+++ src/api/trefle.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://trefle.io/api/v1`,
+  baseURL: `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1`,
   timeout: 1000,
   headers: {
     Authorization: "GQNO3jvMdet-pXOeozOMBjvsP6cuMwGj_an-nSDgYOI",
