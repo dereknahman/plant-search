5e6cb708a78e03df2945a0eccd18ae7ccaf0b834
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-09-30 22:05:15 +0100

    well the api request sort of works but the results have no relationship with the input field - a problem for tomorrow

--- src/api/trefle.js
+++ src/api/trefle.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1`,
-  timeout: 1000,
+  //   timeout: 1000,
   headers: {
     Authorization: "GQNO3jvMdet-pXOeozOMBjvsP6cuMwGj_an-nSDgYOI",
   },
