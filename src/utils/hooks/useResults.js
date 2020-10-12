65d6692c3b90c6bb02c5e05ab10e5d8a3b1154af
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-10-12 16:32:35 +0100

    removed unused console logs

--- src/utils/hooks/useResults.js
+++ src/utils/hooks/useResults.js
@@ -5,10 +5,7 @@ export default () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  console.log("results state var coming from hook", results);
-
   const searchApi = async (term) => {
-    console.log("search", { term });
     try {
       const { data } = await trefle.get(`/plants/search`, {
         params: {
@@ -16,9 +13,7 @@ export default () => {
         },
       });
       setErrorMessage("");
-      console.log("double data from hook", data.data);
       setResults(data.data);
-      // setResults(data.data);
     } catch (err) {
       setErrorMessage("Something went wrong");
     }
